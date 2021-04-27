4bf383efd3c33c09a68ab648b41476e0f8b97dd6
GooS69 <guseff.daniil2011@yandex.ru>
2021-04-27 18:10:29 +0300

    fix /

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('polls.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
