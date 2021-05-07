12e89b7e78769e00272d45c4f88f5a1d2e6643ed
GooS69 <guseff.daniil2011@yandex.ru>
2021-05-07 12:18:53 +0300

    fix settings too

--- mysite/settings.py
+++ mysite/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
